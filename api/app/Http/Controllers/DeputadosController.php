edcc3b3d0e0adb50172a9ff02a7b0d4854d82a81
dan-tovytch <danielmelentovytch@gmail.com>
2025-07-22 22:06:46 -0300

    (refactor): aprimorar metodo index e adicionado metodo de ranking

--- api/app/Http/Controllers/DeputadosController.php
+++ api/app/Http/Controllers/DeputadosController.php
@@ -2,26 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Deputados;
 use Illuminate\Http\Request;
+use App\Jobs\ProcessDeputados;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use App\Jobs\ProcessDeputadoInfoJob;
 use Illuminate\Support\Facades\Http;
+use App\Jobs\ProcessDeputadosInfoJob;
+use Exception;
 
 class DeputadosController extends Controller
 {
     public function index()
     {
-        $response = Http::get("https://dadosabertos.camara.leg.br/api/v2/deputados");
 
-        if ($response->failed()) {
+        Log::info('Iniciando a atualização dos deputados.');
+
+
+        $result = Http::get("https://dadosabertos.camara.leg.br/api/v2/deputados");
+
+        if ($result->failed()) {
+            Log::error('Falha ao buscar deputados da API.');
+
             return response()->json([
                 "error" => true,
                 "message" => "Nenhum dado encontrado!",
             ], 500);
         }
 
-        return response()->json([
-            "error" => false,
-            "message" => "Sucesso!",
-            "data" => $response->json()
-        ], 200);
+
+        $cpf = collect($result->json("dados"))->pluck("cpf");
+        $consult = Deputados::whereIn("cpf", $cpf)->pluck("cpf")->toArray();
+        $cpfsToRegister = $cpf->diff($consult);
+
+        // recebe os id dos deputados a serem registrados, além do cpf
+        $idsToRegister = collect($result->json("dados"))
+            ->whereIn("cpf", $cpfsToRegister)
+            ->pluck("id");
+
+        foreach ($idsToRegister as $id) {
+            ProcessDeputadosInfoJob::dispatch($id);
+        }
+    }
+
+
+    public function ranking()
+    {
+        try {
+            $ranking = DB::table('despesas')
+                        ->join('deputados', 'deputados.id', '=', 'despesas.id_deputado')
+                        ->select(
+                            "deputados.nomeEleitoral",
+                            "deputados.siglaPartido",
+                            "deputados.siglaUf",
+                            DB::raw("SUM(valorLiquido) as total")
+                        )
+                        ->groupBy('deputados.id', 'deputados.nomeEleitoral', 'deputados.siglaPartido', 'deputados.siglaUf')
+                        ->orderByDesc('total')
+                        ->limit(10)
+                        ->get();
+
+            return response()->json([
+                "error" => false,
+                "message" => "Sucesso",
+                "data" => $ranking,
+            ], 200);
+        } catch (Exception $e) {
+            Log::error("[DeputadosController][ranking] Erro ao buscar o ranking do deputados: ERROR:" . $e->getMessage());
+            return response()->json([
+                "error" => true,
+                "message" => "Erro interno. Tente novamente",
+            ], 500);
+        }
     }
 }
