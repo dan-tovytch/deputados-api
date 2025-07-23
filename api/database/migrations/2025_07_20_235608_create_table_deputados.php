a4dbfe832f82b2d1720df523978f5f19c9ca908e
dan-tovytch <danielmelentovytch@gmail.com>
2025-07-22 22:25:13 -0300

    (feat): criado tabela deputados

--- api/database/migrations/2025_07_20_235608_create_table_deputados.php
+++ api/database/migrations/2025_07_20_235608_create_table_deputados.php
@@ -12,14 +12,26 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('deputados', function (Blueprint $table) {
-            $table->id();
-            $table->string("name", 255);
-            $table->string("partyAcronym", 255);
-            $table->string("uf", 2);
-            $table->string("email")->unique()->nullable();
-            $table->text("urlPhoto");
-            $table->index('partyAcronym');
-            $table->index('uf');
+            $table->unsignedBigInteger('id')->primary();
+            $table->string('nomeCivil');
+            $table->string('nomeEleitoral');
+            $table->string('siglaPartido');
+            $table->string('siglaUf', 2);
+            $table->integer('idLegislatura');
+            $table->string('urlFoto')->nullable();
+            $table->string('emailGabinete')->nullable();
+            $table->string('telefoneGabinete')->nullable();
+            $table->string('situacao');
+            $table->string('condicaoEleitoral');
+            $table->string('cpf', 11)->unique();
+            $table->char('sexo', 1);
+            $table->string('urlWebsite')->nullable();
+            $table->date('dataNascimento');
+            $table->date('dataFalecimento')->nullable();
+            $table->string('ufNascimento', 2);
+            $table->string('municipioNascimento');
+            $table->string('escolaridade')->nullable();
+            $table->json('redeSocial')->nullable();
             $table->timestamps();
         });
     }
